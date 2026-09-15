cc28ca1bbf1ae3bcf0e3ab0e793d4b5f0db2ff0b
AcolyteMagna <157872227+AcolyteMagna@users.noreply.github.com>
2026-09-15 13:36:05 -0400

    Cards (Chapter 17-21)

---
+++ Cards (Chapter 17-21)/App Startup/AppLoadingView.swift
@@ -0,0 +1,35 @@
+import SwiftUI
+
+struct AppLoadingView: View {
+    @State private var showSplash = true
+
+    var body: some View {
+        if showSplash {
+            SplashScreen()
+                .ignoresSafeArea()
+                .onAppear {
+                    withAnimation(
+                        .linear(duration: 1.0)
+                            .delay(1.5)
+                    ) {
+                        showSplash = false
+                    }
+                }
+        } else {
+            CardsListView()
+                .transition(
+                    .scale(
+                        scale: 0,
+                        anchor: .top
+                    )
+                )
+        }
+    }
+}
+
+#Preview {
+    AppLoadingView()
+        .environmentObject(
+            CardStore(defaultData: true)
+        )
+}
---
+++ Cards (Chapter 17-21)/Cards/Views/ListSelection.swift
@@ -0,0 +1,37 @@
+import SwiftUI
+
+enum ListState {
+    case list
+    case carousel
+}
+
+struct ListSelection: View {
+    @Binding var listState: ListState
+
+    var body: some View {
+        Picker(
+            selection: $listState,
+            label: Text("")
+        ) {
+            Image(
+                systemName:
+                    "square.grid.2x2.fill"
+            )
+            .tag(ListState.list)
+
+            Image(
+                systemName:
+                    "rectangle.stack.fill"
+            )
+            .tag(ListState.carousel)
+        }
+        .pickerStyle(.segmented)
+        .frame(width: 200)
+    }
+}
+
+#Preview {
+    ListSelection(
+        listState: .constant(.list)
+    )
+}
